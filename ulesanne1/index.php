2a9baa53ad46dbe62f7c98a456ce53cf9f7876f8
kingxfshame <artur.kexpa@gmail.com>
2019-11-14 13:24:24 +0200

    uploadulesanne1

--- ulesanne1/index.php
+++ ulesanne1/index.php
@@ -1,6 +1,7 @@
 <?php 
     $xml = simplexml_load_file("autocompleter.xml");
     $form = "Mees";
+
 if(isset($_GET['x'])){
     switch ($_GET['x']) {
         case 'mehed':
@@ -11,6 +12,7 @@ if(isset($_GET['x'])){
     }
 }
 function poisk(){
+
     $result = array();
     $poisk = $_REQUEST['nimi'];
     $xml = simplexml_load_file("autocompleter.xml");
@@ -31,21 +33,22 @@ function poisk(){
 <head>
     <meta charset="utf-8">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
     <title>AutoCompleter</title>
 </head>
 <body>
     <div class="container">
         <div class="row">
             <div class="col-12">
-                <h1>Esimene nimi XML failis: <?php echo $xml->nimi->emakeelne ?></h1>
+                <h1 style="text-align: center;"><i class="fa fa-diamond" aria-hidden="true"></i>AutoCompleter</h1>
             </div>
+
             <div class="col-12">
                 <hr>
-            <h1 style="margin-left:30vh;">Kõik Nimed XML failis</h1>
+            <h3 style="margin-left:30vh;">Kõik Nimed XML failis</h3>
             <table class="table table-striped">
               <thead>
                 <tr>
-                  <th scope="col">#</th>
                   <th scope="col">Emakeelne nimi</th>
                   <th scope="col">Võõrkeelne nimi</th>
                   <th scope="col">Sugu</th>
@@ -55,7 +58,6 @@ function poisk(){
                   <?php     
                   foreach($xml->nimi as $nimed) {
                         echo '<tr>';
-                        echo '<th scope="row">'.$nimed->attributes() -> id.'</th>';
                         echo '<td>'.$nimed->emakeelne.'</td>';
                         echo '<td>'.$nimed->vorkkeelne.'</td>';
                         echo '<td>'.$nimed->sugu.'</td>';
@@ -68,18 +70,17 @@ function poisk(){
 
             <div class="col-6">
                 <hr>
-                <a href="index.php?x=mehed"><h1>Ainult Mehed</h1></a>
+                <a href="index.php?x=mehed"><h3>Ainult Mehed</h3></a>
             </div>
             <div class="col-6">
                 <hr>
-                <a href="index.php?x=naised"><h1>Ainult Naised</h1></a>
+                <a href="index.php?x=naised"><h3>Ainult Naised</h3></a>
             </div>
             <div class="col-12">
 
                 <table class="table table-striped">
                     <thead>
                     <tr>
-                        <th scope="col">#</th>
                         <th scope="col">Emakeelne nimi</th>
                         <th scope="col">Võõrkeelne nimi</th>
                         <th scope="col">Sugu</th>
@@ -89,8 +90,8 @@ function poisk(){
                     <?php
                     foreach($xml->nimi as $nimed) {
                         if($nimed->sugu == $form){
+
                             echo '<tr>';
-                            echo '<th scope="row">'.$nimed->attributes() -> id.'</th>';
                             echo '<td>'.$nimed->emakeelne.'</td>';
                             echo '<td>'.$nimed->vorkkeelne.'</td>';
                             echo '<td>'.$nimed->sugu.'</td>';
@@ -102,7 +103,12 @@ function poisk(){
                     </tbody>
                 </table>
             </div>
-
+            <div class="col-12">
+                <hr>
+            </div>
+            <div class="col-12">
+                <h3>Esimene nimi XML failis: <?php echo $xml->nimi->emakeelne ?></h3>
+            </div>
             <div class="col-12">
                 <hr>
                 <div class="card">
@@ -123,13 +129,26 @@ function poisk(){
                                 <div class="col-auto">
                                     <button type="submit" class="btn btn-primary mb-2">Otsi Nimi</button>
                                 </div>
+                                <div class="col-auto"><h5><?php
+                                        if(isset($_REQUEST['poisk'])){
+                                            $inimesed = poisk();
+                                            $leidnud = 0;
+                                            foreach($inimesed as $nimed) {
+                                                $leidnud = $leidnud +1;
+
+                                            }
+                                            echo "On leidnud ainult : ".$leidnud." inimesi";
+                                        }
+
+
+
+                                        ?></h5></div>
                             </div>
                         </form>
                         </p>
                         <table class="table table-striped">
                             <thead>
                             <tr>
-                                <th scope="col">#</th>
                                 <th scope="col">Emakeelne nimi</th>
                                 <th scope="col">Võõrkeelne nimi</th>
                                 <th scope="col">Sugu</th>
@@ -139,13 +158,14 @@ function poisk(){
                             <?php
                             if(isset($_REQUEST['poisk'])){
                                 $inimesed = poisk();
+                                $leidnud = 0;
                                 foreach($inimesed as $nimed) {
                                     echo '<tr>';
-                                    echo '<th scope="row">'.$nimed->attributes() -> id.'</th>';
                                     echo '<td>'.$nimed->emakeelne.'</td>';
                                     echo '<td>'.$nimed->vorkkeelne.'</td>';
                                     echo '<td>'.$nimed->sugu.'</td>';
                                     echo '</tr>';
+                                    $leidnud = $leidnud +1;
 
                                 }
                             }
